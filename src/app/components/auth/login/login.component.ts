9e0243125368fc0ab794778ce6b795bef0d7c674
emeery <gerardoemery@live.com.mx>
2022-09-10 23:59:06 -0500

    modal acceso

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form: FormGroup;
+  bar: boolean = false;
+  constructor(
+    private servicioAut: AuthService,
+    private formBuilder: FormBuilder,
+    private dlgRef: MatDialogRef<LoginComponent>
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
+  ngOnInit() {}
+
+  get f() {
+    return this.form.controls;
+  }
+
+  ingreso() {
+    this.bar = true;
+    setTimeout(
+      () => this.servicioAut.iniciarSesion(this.form.value.email),
+      1500
+    );
+    setTimeout(() => this.dlgRef.close(), 1000);
+  }
 }
--- src/app/components/shared/app-progress-bar/app-progress-bar.component.ts
+++ src/app/components/shared/app-progress-bar/app-progress-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app-progress-bar',
+  selector: 'app-progress-bar',
   templateUrl: './app-progress-bar.component.html',
   styleUrls: ['./app-progress-bar.component.scss']
 })
