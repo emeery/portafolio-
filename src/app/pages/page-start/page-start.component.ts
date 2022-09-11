1a45064ffa20925e957f4bfd77c8cb4d6a912129
emeery <gerardoemery@live.com.mx>
2022-09-11 00:39:10 -0500

    pagina bienvenida

--- src/app/pages/page-start/page-start.component.ts
+++ src/app/pages/page-start/page-start.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-page-start',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-start.component.scss']
 })
 export class PageStartComponent implements OnInit {
+  todos: string[] = [
+    'bun venit', 'bienvenue', '歡迎', 'welcome', 'welkom', 'benvenuto', 'fáilte', 'bienvenido', 'a', 'mi', 'portafalio', ':)'];
+  saludo!: string
+  despido= false
+  despidoSub = Subscription
+  constructor(private servicioAut: AuthService) { }
 
-  constructor() { }
+  ngOnInit() { 
+    this.checaSaludo()
+  }
 
-  ngOnInit(): void {
+  checaSaludo() {
+    this.despido = this.servicioAut.obtieneDespedida()
+    console.log(this.despido)
+    if(this.despido) { this.saludo = 'vuelve pronto!' } 
+    else this.bienvenida()
+  }
+
+  despedida() {
+    this.saludo = 'vuelve pronto!'
+  }
+
+  bienvenida() {
+    const source = interval(500)
+    const subscribe = source.subscribe(i => {
+      this.saludo = this.todos[i]
+    });
+    setTimeout(() => subscribe.unsubscribe(), 6500)
   }
 
 }
