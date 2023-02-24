2e8b8ae038de95214eda51979c11344af9c35d3f
emeery <gerardoemery@live.com.mx>
2023-02-24 12:12:04 -0600

    env variables github pages

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -54,7 +54,6 @@ export class AuthService {
   }
 
   ingreso(user: string) {
-    console.log('usrr', user);
     return this.http
       .post(BACKEND_URL.API_SIGNIN, user)
       .subscribe({
@@ -89,23 +88,6 @@ export class AuthService {
 
   }
 
-  errorHandler(error: any) {
-    console.log(error);
-    if (error.error instanceof ErrorEvent) {
-
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
-  }
 
   cerrarSesion(): void {
     this.token = '';
